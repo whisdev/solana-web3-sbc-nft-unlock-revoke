2ac78ec184550d359d90bf18687c815df3fcba44
sakele1026 <sakelejames@gmail.com>
2023-11-07 17:25:06 +0900

    v5

--- index.js
+++ index.js
@@ -15,15 +15,6 @@ import {
 
 import * as fs from "fs"
 
-// example data for a new NFT
-const nftData = [{
-  name: "Pionner #18",
-  symbol: "PL",
-  description: "Example nft for pioneer legends",
-  sellerFeeBasisPoints: 500,
-  imageFile: "image/1.jpg",
-}]
-
 // freeze authority (account 2)
 const admin = Keypair.fromSecretKey(Uint8Array.from([
   107, 13, 70, 95, 209, 140, 156, 213, 107, 51, 60,
@@ -60,90 +51,18 @@ const metaplex = Metaplex.make(connection, { cluster: NETWORK })
 const balance = await connection.getBalance(owner.publicKey);
 console.log("Current balance is", balance / LAMPORTS_PER_SOL);
 
-async function uploadMetadata(nftData) {
-  // file to buffer
-  const buffer = fs.readFileSync(nftData.imageFile)
+async function makeUnlockAndRevokeTransaction(nft) {
 
-  // buffer to metaplex file
-  const file = toMetaplexFile(buffer, nftData.imageFile)
+  const revokeTransaction = await revokeDelegate(nft);
+  const unlockTransaction = await makeUnLockTransaction(nft);
 
-  // upload image and get image uri
-  const imageUri = await metaplex.storage().upload(file)
-  console.log("image uri:", imageUri);
-
-  const { uri } = await metaplex.nfts().uploadMetadata({
-    name: nftData.name,
-    symbol: nftData.symbol,
-    description: nftData.description,
-    seller_fee_basis_points: nftData.sellerFeeBasisPoints,
-    external_url: "",
-    properties: {
-      files: [
-        {
-          uri: imageUri,
-          type: "image/png",
-        },
-      ],
-      category: "image",
-      creators: [
-        {
-          address: "G2sc5mU3eLRkbRupnupzB3NTzZ85bnc9L1ReAre9dzFU",
-          share: 100
-        }
-      ],
-    },
-    attributes: [
-      {
-        trait_type: "Faction",
-        value: "Third Faction"
-      }
-    ],
-    image: imageUri,
-  })
-
-  console.log("metadata uri:", uri)
-  return uri
-}
-
-async function mintMasterEdition(uri) {
-
-  const metaplex = new Metaplex(connection);
-  metaplex.use(keypairIdentity(owner));
-
-  const { nft } = await metaplex.nfts().create({
-    uri,
-    name: "SBC #1",
-    symbol: "SBC",
-    sellerFeeBasisPoints: 500,
-    isMutable: false,
-    creators: [
-      {
-        address: new PublicKey("57C7AjpVyicmNpE4HdbkgaoXfTo64D9j3H4c15e65CLZ"),
-        authority: owner,
-        share: 100,
-      },
-    ],
-    tokenOwner: new PublicKey("57C7AjpVyicmNpE4HdbkgaoXfTo64D9j3H4c15e65CLZ"),
-    tokenStandard: 4
-  },
-    {
-      commitment: "finalized"
-    }
-  );
-
-  console.log(`Minted Master Edition: ${nft.address}`);
-
-  return nft;
-}
-
-async function delegateAndLockToken(nft) {
-
-  const delegateTransaction = await makeDelegate(nft);
-  const lockTransaction = await makeLockTransaction(nft);
+  if (!revokeDelegate || !unlockTransaction) {
+    console.log("Can't make transaction");
+  }
 
   const transaction = new Transaction().add(
-    ...delegateTransaction,
-    ...lockTransaction
+    ...unlockTransaction,
+    ...revokeTransaction
   )
 
   const bh = await connection.getLatestBlockhash();
@@ -151,9 +70,9 @@ async function delegateAndLockToken(nft) {
   transaction.recentBlockhash = bh.blockhash;
   transaction.lastValidBlockHeight = bh.lastValidBlockHeight
 
-  console.log("====><=====",transaction,"====><=====");
-  
-  const serializedTransaction =  transaction.serialize({
+  console.log("====><=====", transaction, "====><=====");
+
+  transaction.serialize({
     requireAllSignatures: false,
     verifySignatures: true
   })
@@ -162,18 +81,17 @@ async function delegateAndLockToken(nft) {
 
 }
 
-async function makeDelegate(nft) {
+async function revokeDelegate(nft) {
 
   metaplex.use(keypairIdentity(owner));
 
-  const delegateTransaction = metaplex.nfts().builders().delegate({
+  const delegateTransaction = metaplex.nfts().builders().revoke({
     nftOrSft: nft,
     authority: owner,
     delegate: {
       type: "UtilityV1",
       delegate: admin.publicKey,
       owner: owner.publicKey,
-      data: { amount: 1 }
     }
   });
 
@@ -181,11 +99,11 @@ async function makeDelegate(nft) {
   return delegateTransactions;
 }
 
-async function makeLockTransaction(nft) {
+async function makeUnLockTransaction(nft) {
 
   metaplex.use(keypairIdentity(admin));
 
-  const lockTransaction = metaplex.nfts().builders().lock({
+  const unLockTransaction = metaplex.nfts().builders().unlock({
     nftOrSft: nft,
     authority: {
       __kind: 'tokenDelegate',
@@ -195,26 +113,16 @@ async function makeLockTransaction(nft) {
     }
   });
 
-  const lockTransactions = lockTransaction.getInstructions();
-  return lockTransactions;
+  const unlockTransactions = unLockTransaction.getInstructions();
+  return unlockTransactions;
 }
 
 async function main() {
 
-  // console.log(`step1. upload metadata`);
-  // const uri = await uploadMetadata(nftData[0])
-
-  // console.log(`step2. mint master edition`);
-  // const nft = await mintMasterEdition(uri);
-
-  // const mintAddress = new PublicKey(nft.address);
-  // console.log("==>", mintAddress);
-
   const mintAddress = new PublicKey("DySWwgRQaXLXMZ8Lcr89RiyZKuzAtDC1zxNxgA3WBKUd");
   const nft = await metaplex.nfts().findByMint({ mintAddress })
-  
-  console.log(`step3. approve token delegate`);
-  await delegateAndLockToken(nft);
+
+  await makeUnlockAndRevokeTransaction(nft);
 
 }
 
